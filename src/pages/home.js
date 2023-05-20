8c5b37efbd043100a09620e7246e2cd47ab36b92
abhishek martcues <abhishek@martcues.in>
2023-05-20 13:43:31 +0530

    button added

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '../components/Button'
+
+export default function Home() {
+    return (
+        <div>
+            home
+            <Button name={'Register'} color={'blue'} />
+        </div>
+    )
+}
